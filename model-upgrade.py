f43d1443f9059b1dd0d097df17358a7e762974c4
Cyrax321 <cyrax8590@gmail.com>
2026-04-12 14:26:10 +0530

    Add model-upgrade.py

---
+++ model-upgrade.py
@@ -0,0 +1,5 @@
+model_name = 'DeepNet-1'
+loss = 0.25
+accuracy = 0.85
+performance = accuracy - loss
+print(f"model{model_name} has a loss of {loss} , an accuracy of {accuracy} and performance of {performance}")
