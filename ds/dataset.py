d36de679b2a507b0053a8857796a5bad5ee82e7b
Cyrax321 <cyrax8590@gmail.com>
2026-04-13 09:09:55 +0530

    Add dataset definition in ds/dataset.py

---
+++ ds/dataset.py
@@ -0,0 +1,9 @@
+raw_data = [1.2, 2.5, 0.8, 10.0, 15.1, 0.1]
+clean_data = raw_data[0:3]
+labels_map = {
+    "success"  : True,
+    "failure " : False
+}
+
+print(f'last item in raw_data is {raw_data[-1]}')
+print(labels_map['success'])
