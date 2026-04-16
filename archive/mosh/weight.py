9e3dd3c906fa019b04ae7244b8c0f8bbfe316ffd
Cyrax321 <cyrax8590@gmail.com>
2026-04-16 15:46:19 +0530

    Add logic: weight.py [7]

--- archive/mosh/weight.py
+++ archive/mosh/weight.py
@@ -4,3 +4,4 @@ data = str(input("Enter the unit (kg(k) or lbs(l)) : "))
 if data == "kg" or data == "k":
     lbs = weight * 2.20462 
     print(f"Your weight in lbs is : {lbs}")
+elif data == "lbs" or data == "l":
