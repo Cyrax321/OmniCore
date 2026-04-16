63f5a57930fb8196158f942b9ff3bb2ea8fb935d
Cyrax321 <cyrax8590@gmail.com>
2026-04-16 15:04:53 +0530

    Add logic: conditionals.py [9]

--- archive/mosh/conditionals.py
+++ archive/mosh/conditionals.py
@@ -6,3 +6,4 @@ product = input("Enter the product (use the terms good,bad and ok ) : ")
 if price < 10 and product == "good":
     print("Buy it ")
 elif price > 10 and product == "bad":
+    print("Don't buy it")
