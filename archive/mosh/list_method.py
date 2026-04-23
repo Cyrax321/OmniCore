b960830f895755f22006b6c370f6a4bbfe0258ca
Cyrax321 <cyrax8590@gmail.com>
2026-04-23 13:14:37 +0530

    chore(mosh): add list methods practice

---
+++ archive/mosh/list_method.py
@@ -0,0 +1,17 @@
+numbers = [1,2,3,3,4,5,6,7,8]
+
+# append
+numbers.append(6)
+print(numbers)
+
+# insert
+numbers.insert(0,9)
+print(numbers)
+
+#remove
+numbers.remove(3)
+print(numbers)
+
+#clear 
+numbers.clear()
+print(numbers)
