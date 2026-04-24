7895b6221d36ebebfe9cabbeb9613540dddeddb6
Cyrax321 <cyrax8590@gmail.com>
2026-04-24 09:05:29 +0530

    docs(research): add Harvard CS50 string manipulation practice

---
+++ harvard-cs50/simplified-title-maker.py
@@ -0,0 +1,5 @@
+#chaining
+title = input("what is your title? : ").title().strip()
+
+print(f'The enhanced title is : {title}')
+
---
+++ harvard-cs50/str.py
@@ -0,0 +1,8 @@
+name = input("what is your name? ")
+
+#remove white space
+name = name.strip()
+#capitalize 
+name = name.title()
+
+print(f'Hello, {name}')  
