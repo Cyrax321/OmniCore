f26cef1fb42d7c2b4864ba36bf2e3b0d990f41c2
Cyrax321 <cyrax8590@gmail.com>
2026-04-24 09:08:19 +0530

    feat(ds): add list structure benchmark scripts

---
+++ ds/list.py
@@ -0,0 +1,15 @@
+list = ['fish','cat','dog','monkey']
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+
+data = [1,2,3,4,5,6,7,8,9,10]
+
+first_3 = data[0:3]
+print(first_3)
+
+third_to_end = data[2:]
+print(third_to_end)
+
+
