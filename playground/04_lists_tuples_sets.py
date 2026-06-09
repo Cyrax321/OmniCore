4fb2eb87758620e137cb2a48c99750a150c7882b
Cyrax321 <cyrax8590@gmail.com>
2026-06-09 14:10:13 +0530

    feat: add list indexing and modification practice in playground

--- playground/04_lists_tuples_sets.py
+++ playground/04_lists_tuples_sets.py
@@ -1,1 +1,19 @@
-# Corey Schafer Python: 04_lists_tuples_sets
+courses = ["python","history","math","physics"]
+print(courses)
+print("The First Course is:", courses[0])
+print('alternating courses:', courses[::2])
+print("The last Courses are:", courses[2:])
+
+
+#list methods 
+''' 
+1. append() 
+2. insert() #specific position insert(2,"Art")
+
+'''
+
+
+courses.append("chem")
+
+courses.insert(3,"literature")
+print(courses)
