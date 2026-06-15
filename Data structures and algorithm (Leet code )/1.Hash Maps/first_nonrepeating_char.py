c2339dfe31fd1dfa3e69003acc38cfd8919cc449
Cyrax321 <cyrax8590@gmail.com>
2026-06-16 02:06:18 +0530

    Fix formatting and update test case in first_nonrepeating_char.py

--- Data structures and algorithm (Leet code )/1.Hash Maps/first_nonrepeating_char.py
+++ Data structures and algorithm (Leet code )/1.Hash Maps/first_nonrepeating_char.py
@@ -1,10 +1,14 @@
-def first_non_repeating_char(string):
+def nonrepeat(string):
     freq = {}
-    for x in string:
+
+    for x in string :
         freq[x] = freq.get(x,0) + 1
+
     for i in string :
         if freq[i] == 1:
-            return i
-    return -1
-print(f"the first non-repeating char is: {first_non_repeating_char("leetcode")}")
+            return i 
+    return -1 
 
+print(f" The first non repeating character is {nonrepeat('abcabc')}")
+    
+    
